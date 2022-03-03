85ade1a2f4d1760fa2838946cd2fd58459d7e4fd
murgendrag <murgendra67@gmail.com>
2022-03-04 00:15:03 +0530

    program file added

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace leapyear
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //class1.checkleapyear();
+            // flipcoin.headtailpercentage();
+            //powerof2.tableofpowerof2();
+            // harmonic_mean.calculateHM();
+            primefactors.computeprimefactors();
+        }
+    }
+
+}
---
+++ flipcoin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leapyear
+{
+    public class flipcoin
+    {
+        public static void headtailpercentage()
+        {
+            Console.WriteLine("Enter the no of times to flip the coin");
+            int flip = Convert.ToInt32(Console.ReadLine());
+
+            if (flip > 0) ;
+            {
+                int head = 1;
+                int tail = 1;
+                int headcount = 0;
+                int tailcount = 0;
+                double headpercentage;
+                double tailpercentage;
+
+                for (int i = 1; i <= flip; i++)
+                {
+                    Random random = new Random();
+                    int number = random.Next(2);
+                    Console.WriteLine(number);
+                    if (number == 0)
+                    {
+                        tailcount += tail;
+                    }
+
+                    else
+                    {
+                        headcount += head;
+                    }
+
+                }
+                tailpercentage = (tailcount * 100) / flip;
+                headpercentage = (headcount * 100) / flip;
+
+                Console.WriteLine("headpercentage=" + headpercentage);
+                Console.WriteLine("tailpercentage=" + tailpercentage);
+            }
+        }
+    }
+}
