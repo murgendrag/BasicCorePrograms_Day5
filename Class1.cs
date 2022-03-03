0d67fef7ad43930a68214c8e1dfb3b3b4d331b09
murgendrag <murgendra67@gmail.com>
2022-03-04 00:19:07 +0530

    leapyear program added

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leapyear
+{
+    public class class1
+    {
+        public static void checkleapyear()
+
+        {
+            Console.WriteLine("program to find whether year is leap year or not");
+            Console.WriteLine("enter the year YYYY format");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if ((year%4==0) && (year%100!=0) || (year % 400==0))
+            {
+                Console.WriteLine(year + "year is leap year");
+
+            }
+            else
+            {
+                Console.WriteLine(year + "year is not a leap year");
+            }
+        }
+     }
+}
