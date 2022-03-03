408a735de52779d7451bc2ee27428577033647aa
murgendrag <murgendra67@gmail.com>
2022-03-04 00:31:26 +0530

    harmonicmean program added

---
+++ harmonic mean.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leapyear
+{
+     class harmonic_mean
+    {
+        public static void calculateHM()
+        {
+            Console.WriteLine("program to calculate harmonic series and mean upto nth term");
+            Console.WriteLine("enter the value of n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i;
+            double s = 0;
+            for (i = 1; i <= n; i++)
+            {
+                Console.WriteLine("1/" + i);
+               s+=1/(float)i;
+            }   
+            
+            Console.WriteLine("harmonic mean upto "  + n +"  "  + s);
+        }
+    }
+}
