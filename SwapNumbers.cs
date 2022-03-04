b2b0d044c3de941d7c60127c5ec1fe0fe0da49b1
murgendrag <murgendra67@gmail.com>
2022-03-04 18:22:08 +0530

    swap two numbers

---
+++ SwapNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leapyear
+{
+    public class SwapNumbers
+    {
+        public static void SwaptwoNumbers()
+        {
+            int a, b;
+            Console.WriteLine("Enter the number for variable a");
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number for variable b");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Before swapping numbers are a="+a +"b=" +b);
+
+            a = a * b;  //swapping of numbers
+            b= a / b;
+            a = a / b;
+
+            Console.WriteLine("After swapping numbers are a=" + a + "b=" + b);
+        }
+    }
+}
