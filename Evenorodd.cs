ed472857bf96753b4ccbf58aaf70eaa67c80502d
murgendrag <murgendra67@gmail.com>
2022-03-04 18:33:34 +0530

    Checking number is even or odd

---
+++ Evenorodd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leapyear
+{
+    public class Evenorodd
+    {
+        public static void CheckEvenorOdd()
+        {
+            int number;
+            Console.WriteLine("Enter number");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if(number%2==0)
+            {
+                Console.WriteLine("The Number "+number +"is even number");
+            }
+            else
+            {
+                Console.WriteLine("The Number " + number + "is odd number");
+            }
+        }
+    }
+}
