7c5b01c62c4a3f57ce48415e49e3cbe95284d37f
murgendrag <murgendra67@gmail.com>
2022-03-04 18:58:24 +0530

    check vowel or Consonant

---
+++ VowelsorConsonant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leapyear
+{
+    public class VowelsorConsonant
+    {
+        public static void CheckVowelorConsonant()
+        {
+            char alpha;
+            Console.WriteLine("Enter alphabet to check Vowel or Consonant");
+            alpha = Convert.ToChar(Console.ReadLine());    
+
+            if (alpha=='a' || alpha == 'e' || alpha == 'i' || alpha == 'o' || alpha == 'u')
+            {
+                Console.WriteLine( alpha +" is Vowel");
+            }
+            else
+            {
+                Console.WriteLine(alpha + "This is Consonant");
+
+            }
+        }
+    }
+}
